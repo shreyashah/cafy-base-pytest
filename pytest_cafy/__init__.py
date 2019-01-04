1036f83bec8ed7a4911e20b7bcc7b3e91a4fb1ff
Amrendra Kumar <kuamrend@cisco.com>
2019-01-04 15:45:59 -0800

    Add version information to repo

--- pytest_cafy/__init__.py
+++ pytest_cafy/__init__.py
@@ -3,3 +3,6 @@ Created on Feb 16, 2017
 
 @author: Fahad Naeem Khan
 '''
+
+from .__version__ import __version__
+print("Cafy: %s" % __version__) 
--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 '''
 Created on Feb 16, 2017
 
-@author: fahad naeem khan
+@author: Cafy
 '''
 # sample ./setup.py file
 from setuptools import setup
 
+from pytest_cafy.__version__ import __version__
+#__version__ = "0.1.0"  
 
 setup(
     name="pytest_cafy",
     packages=['pytest_cafy'],
     package_data={'pytest_cafy': ['resources/*']},
-    author='Fahad Naeem Khan',
-    author_email='fahadnaeemkhan@gmail.com',
+    author='Cafy',
+    author_email='cafy-support@cisco.com',
+    version=__version__,
+    license="GPLv2",
+    url='https://github.com/kuamrend/cafy-pytest',
+    description='Pytest Cafy Plugin', 	
     install_requires=['pytest>=2.3', 'jinja2'],
     # the following makes a plugin available to pytest
     entry_points={
