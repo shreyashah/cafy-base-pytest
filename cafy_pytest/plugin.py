16e2bfcb661c286315b9b7052f3ff61da5b2dd0e
zoykhan <32883901+zoykhan@users.noreply.github.com>
2020-08-20 06:03:48 -0700

    Update plugin.py

--- cafy_pytest/plugin.py
+++ cafy_pytest/plugin.py
@@ -1299,7 +1299,7 @@ class EmailReport(object):
                                               "exception_name": rc_exception_name_list}
                                     response = self.invoke_rc_on_failed_testcase(params, headers)
                                     if response is not None and response.status_code == 200:
-                                        if response.json().get("traffic_logs")::
+                                        if response.json().get("traffic_logs"):
                                             self.rclog.info("Debug RC logs: \n%s" % response.json()["traffic_logs"])
                     else:
                         self.log.debug("Type of report obtained is %s. Debug engine is only triggered for reports of type TestReport" %type(report))
