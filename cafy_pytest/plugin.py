8956e8ac5e721441d56eae3210b3d409f26683ee
pasverma <pasverma@cisco.com>
2023-01-26 01:16:10 +0530

    .

--- cafy_pytest/plugin.py
+++ cafy_pytest/plugin.py
@@ -1220,6 +1220,9 @@ class EmailReport(object):
         if report.when == 'setup':
             self.log.set_testcase(testcase_name)
             self.log.title("Start test:  %s" %(testcase_name))
+            if self.lsan:
+               self.collection_manager.connect()
+               self.collection_manager.configure()
             #Notify testcase_name to handshake server
             #If config.debug_enable is False, the reg_dict is empty, So u want to skip talking to handshake server
             if self.reg_dict:
@@ -1340,9 +1343,7 @@ class EmailReport(object):
                 self.model_coverage_report[testcase_name]=CafyLog.model_tracker_dict
                 CafyLog.model_tracker_dict={}
             if self.lsan:
-               self.log.info(self.collection_manager)
-               self.collection_manager.configure()
-
+               self.collection_manager.disconnect()
             self.log.title("Finish test: %s (%s)" %(testcase_name,status))
             self.log.info("="*80)
 
